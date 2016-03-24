6065492a9410d7a440d7a5581b6d287d011a16a2
anhhaonguyen <haonguyen1008@gmail.com>
2016-03-24 02:30:39 -0400

    add 2 port

---
+++ testSocket-3000.js
@@ -0,0 +1,48 @@
+var WebSocketServer = require('websocket').server;
+var http = require('http');
+
+var clients = [];
+
+var server = http.createServer(function(request, response) {
+	console.log((new Date()) + ' Received request for ' + request.url);
+	response.writeHead(404);
+	response.end();
+});
+
+server.listen(3000, function() {
+	console.log((new Date()) + ' Server is listening on port 3000');
+});
+
+wsServer = new WebSocketServer({
+	httpServer: server,
+	autoAcceptConnections: false
+});
+
+function originIsAllowed(origin){
+	return true;
+}
+
+wsServer.on('request', function(request) {
+	if(!originIsAllowed(request.origin)) {
+		request.reject();
+		console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected');
+		return;
+	}
+	var connection = request.accept('', request.origin);
+	clients.push(connection);
+
+	console.log((new Date()) + ' Connection accepted');
+	connection.on('message', function(message) {
+		if (message.type==='utf8') {
+			console.log('Received message: ' + message.utf8Data);
+			clients.forEach(function (client) {
+				client.send(message.utf8Data);
+			});
+		} else if (message.type==='binary') {
+			console.log('Received binary: ' + message.binaryData.length + ' bytes');
+		}
+	});
+	connection.on('close', function(reasonCode, description) {
+		console.log((new Date()) + ' Peer' + connection.remoteAddress + ' disconnected');
+	});
+});
